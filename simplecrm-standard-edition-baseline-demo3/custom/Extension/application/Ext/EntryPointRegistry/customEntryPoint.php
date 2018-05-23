0c78cd207001de63dec16d5e0377188326bcc626
sonu4 <kotheshubham4@gmail.com>
2018-05-23 12:51:01 +0530

    Entry Points

---
+++ simplecrm-standard-edition-baseline-demo3/custom/Extension/application/Ext/EntryPointRegistry/customEntryPoint.php
@@ -0,0 +1,11 @@
+<?php
+
+$entry_point_registry['MyEntryPoint'] = array(
+        'file' => 'custom/custom_entry_points/entryPoint.php',
+        'auth' => true
+);
+
+$entry_point_registry['CreateBooks'] = array(
+        'file' => 'custom/custom_entry_points/createBooks.php',
+        'auth' => true
+);
